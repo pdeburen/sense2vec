0027b8fc4d7b007de9fd6b828feade29fcb816bd
Pascal de Buren <pdeburen@gmail.com>
2016-08-24 08:29:00 +0200

    instream

--- bin/merge_text.py
+++ bin/merge_text.py
@@ -148,9 +148,9 @@ def main(in_loc, out_dir, n_workers=4, n_threads=1, batch_size=10000, load_parse
     #else:
     textfiles = [path.join(in_loc, fn) for fn in os.listdir(in_loc)]
     if n_workers >= 2:
-        jobs = partition(200000, textfiles)
+        #jobs = partition(200000, textfiles)
         do_work = parse_and_transform
-        parallelize(do_work, enumerate(jobs), n_workers, [out_dir, n_threads, batch_size],backend='multiprocessing')
+        parallelize(do_work, enumerate(textfiles), n_workers, [out_dir, n_threads, batch_size],backend='multiprocessing')
     else:
         [parse_and_transform(0, file, out_dir, n_threads, batch_size) for file in textfiles]
 
